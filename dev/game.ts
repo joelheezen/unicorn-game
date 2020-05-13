12e0147316335affe5f01b6f12ab766a08d64c2e
Luuk 's-Gravendijk <0966746@hr.nl>
2020-05-13 12:17:24 +0200

    Working TypeScript setup

---
+++ dev/game.ts
@@ -0,0 +1,7 @@
+class Game {
+    constructor() {
+        console.log("testing games")
+    }
+}
+
+window.addEventListener("load", () => new Game())
---
+++ docs/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+var Game = (function () {
+    function Game() {
+        console.log("testing games");
+    }
+    return Game;
+}());
+window.addEventListener("load", function () { return new Game(); });
+//# sourceMappingURL=main.js.map
