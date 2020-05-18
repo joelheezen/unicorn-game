de5a8256fb552fe4deba7d4bba6c52010df284b0
Tom Faust <tom.faust@hotmail.com>
2020-05-18 09:45:01 +0200

    added background to game

--- dev/game.ts
+++ dev/game.ts
@@ -3,8 +3,10 @@ class Game {
         console.log("Class Game Loaded")
         let background = document.createElement("background")
         let game = document.getElementsByTagName("game")[0]
-
+        background.style.backgroundImage = "url(../docs/assets/2.png)"
         game.appendChild(background)
+
+       
     }
 }
 
