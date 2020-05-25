ec05aefce3fa7f6bf1844f32cd380b87cc288f58
joel <joel_heezen96@hotmail.com>
2020-05-25 12:50:16 +0200

    added turns and random placement of enemies

--- dev/battlephase.ts
+++ dev/battlephase.ts
@@ -19,7 +19,7 @@ class BattlePhase{
             }
         });
 
-        //sets a new background
+        // sets a new background
         let background = document.createElement("background")
         background.style.backgroundImage = "url(assets/2.png)"
         game.appendChild(background)
@@ -29,7 +29,14 @@ class BattlePhase{
         let squares = 140
         let xPosSquare = 0
         let yPosSquare = 0
+        // creates an array of the spaces so you can make a layout of each map
+        let spaces = document.getElementsByTagName("moveSpace")
+        // new array to store the monsters in per level
+        let monsters = new Array
+        let monsterCount
         let inventory = document.getElementsByTagName('inventory')[0]
+        // stage number for testing
+        let stage = 6
 
 
         function allowDrop(ev: any) {
@@ -75,8 +82,161 @@ class BattlePhase{
             inventoryItems[i].draggable = true
             inventoryItems[i].addEventListener("dragstart",() => drag(event))
         }
-    }
-    createBoard(){
-        
-    }
+
+        if (stage == 1) {
+            monsterCount = 4
+            for (let i = 0; i < monsterCount; i++) {
+                let monster = document.createElement("monster")
+                monster.id = "monster" + i
+                monsters.push(monster)
+            }
+            for (let i = 0; i < monsterCount; i++) {
+                let randomNumber = Math.floor(Math.random() * 70)
+                console.log(randomNumber)
+                if(spaces[randomNumber].firstChild){
+                    i -= 1
+                }
+                else {
+                    spaces[randomNumber].appendChild(monsters[i])
+                }
+            }
+        }
+
+        if (stage == 2) {
+            monsterCount = 4
+            for (let i = 0; i < monsterCount; i++) {
+                let monster = document.createElement("monster")
+                monster.id = "monster" + i
+                monsters.push(monster)
+            }
+            for (let i = 0; i < monsterCount; i++) {
+                let randomNumber = Math.floor(Math.random() * 70)
+                console.log(randomNumber)
+                if(spaces[randomNumber].firstChild){
+                    i -= 1
+                }
+                else {
+                    spaces[randomNumber].appendChild(monsters[i])
+                }
+            }
+        }
+
+        if (stage == 3) {
+            monsterCount = 4
+            for (let i = 0; i < monsterCount; i++) {
+                let monster = document.createElement("monster")
+                monster.id = "monster" + i
+                monsters.push(monster)
+            }
+            for (let i = 0; i < monsterCount; i++) {
+                let randomNumber = Math.floor(Math.random() * 70)
+                console.log(randomNumber)
+                if(spaces[randomNumber].firstChild){
+                    i -= 1
+                }
+                else {
+                    spaces[randomNumber].appendChild(monsters[i])
+                }
+            }
+        }
+
+        if (stage == 4) {
+            monsterCount = 4
+            for (let i = 0; i < monsterCount; i++) {
+                let monster = document.createElement("monster")
+                monster.id = "monster" + i
+                monsters.push(monster)
+            }
+            for (let i = 0; i < monsterCount; i++) {
+                let randomNumber = Math.floor(Math.random() * 70)
+                console.log(randomNumber)
+                if(spaces[randomNumber].firstChild){
+                    i -= 1
+                }
+                else {
+                    spaces[randomNumber].appendChild(monsters[i])
+                }
+            }
+        }
+
+        if (stage == 5) {
+            monsterCount = 4
+            for (let i = 0; i < monsterCount; i++) {
+                let monster = document.createElement("monster")
+                monster.id = "monster" + i
+                monsters.push(monster)
+            }
+            for (let i = 0; i < monsterCount; i++) {
+                let randomNumber = Math.floor(Math.random() * 70)
+                console.log(randomNumber)
+                if(spaces[randomNumber].firstChild){
+                    i -= 1
+                }
+                else {
+                    spaces[randomNumber].appendChild(monsters[i])
+                }
+            }
+        }
+
+        if (stage == 6) {
+            monsterCount = 4
+            for (let i = 0; i < monsterCount; i++) {
+                let monster = document.createElement("monster")
+                monster.id = "monster" + i
+                monsters.push(monster)
+            }
+            for (let i = 0; i < monsterCount; i++) {
+                let randomNumber = Math.floor(Math.random() * 70)
+                console.log(randomNumber)
+                if(spaces[randomNumber].firstChild){
+                    i -= 1
+                }
+                else {
+                    spaces[randomNumber].appendChild(monsters[i])
+                }
+            }
+            enemyTurn()
+        }
+
+        function enemyTurn(){
+            // this piece of code is to check the else loop of the next if statement
+            /*let toDelete = document.getElementById("monster0")
+            if (toDelete != null && toDelete.parentNode != null){
+            toDelete.parentNode.removeChild(toDelete)
+            } */ 
+
+            // if the boss lives the enemy turn starts
+            if (document.getElementById("monster0")) {
+                console.log("the enemies are advancing")
+                let monstersLeft = document.getElementsByTagName("monster")
+                let activeMonster = document.getElementById("monster" + Math.floor(Math.random() * monstersLeft.length))
+                if (activeMonster != null) {
+                    let spaceNow = activeMonster.parentNode as Element
+                    console.log(spaceNow.id)
+                    let direction = Math.floor(Math.random() * 100)
+                    if (direction < 10){
+                        console.log("move back")
+                    }
+                    else if(direction < 20 && direction >= 10) {
+                        console.log("move left")
+                    }
+                    else if(direction < 30 && direction >= 20) {
+                        console.log("move right")
+                    }
+                    else {
+                        console.log("move forward")
+                    }
+                }
+                console.log(activeMonster)
+                //playerTurn()
+            } // otherwise you should have won and the game advances to the next level
+            else {
+                console.log("you won")
+            }
+        }
+
+        function playerTurn() {
+            enemyTurn()
+        }
+    }        
 }
